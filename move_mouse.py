e2fca51f4a7c1939a202a23fd9a5f41a48120539
dig <70334760+diganta121@users.noreply.github.com>
2024-10-21 02:01:03 +0530

    something done but broken

--- move_mouse.py
+++ move_mouse.py
@@ -1,18 +1,32 @@
 from pynput.keyboard import Key, Listener
 import pyautogui
 
+data = {'x':-100,
+        'y':-1,
+        'duration':0.1,
+        'enbl':False,
+        }
+
+
 def show(key):
+    #press tab to enable / disable
     if key == Key.tab:
-        print("enabled")
-         
-    if key != Key.tab:
-        print("e")
-         
-    # by pressing 'delete' button 
+        data['enbl'] = not data['enbl']
+        print(data['enbl'])
+    if data['enbl']: 
+        # if space is pressed press w 
+        # for testing
+        if key == Key.space:
+            pyautogui.press('w')
+            pyautogui.moveRel(data['x'],data['y'],duration=data['duration'])
+
+
+    # by pressing 'delete' button 8
     # you can terminate the loop 
     if key == Key.delete: 
         return False
  
-# Collect all event until released
+
+# main loop
 with Listener(on_press = show) as listener:
     listener.join()
